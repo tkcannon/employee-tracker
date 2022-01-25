a4573a0fa555d41f2a5919ac975335ede1064b38
tkcannon <taylorkcannon@gmail.com>
2022-01-25 15:18:10 -0700

    created schema

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,3 @@
-DROP DATABASE IF EXISTS employee-tracker;
-CREATE DATABASE employee-tracker;
-USE employee-tracker;
+DROP DATABASE IF EXISTS employeeTracker;
+CREATE DATABASE employeeTracker;
+USE employeeTracker;
